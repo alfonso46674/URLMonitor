60090b1c17088b3918ac05de13e7bd892ed7c24c
alfonso.ramirez <is713932@iteso.mx>
2021-07-27 17:37:15 -0500

    Docs: Moved config from root to lib/

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const https = require('https');
 const {URL,URLSearchParams} = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const fs = require('fs')
-const config = require('./config');
+const config = require('./lib/config');
 const handlers = require('./lib/handlers')
 const helpers = require('./lib/helpers')
 
