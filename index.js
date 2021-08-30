ab24337ed525701e1cdacafc67a17ee0601f4d80
alfonso.ramirez <is713932@iteso.mx>
2021-08-30 00:59:39 -0500

    Feat: Add basic CLI scaffolding

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli')
 
 //declare application
 let app = {};
@@ -13,6 +14,12 @@ app.init = function(){
     server.init();
     //start workers
     workers.init();
+
+    //start the cli, make sure it starts last
+    setTimeout(function(){
+        cli.init();
+    },50);
+
 };
 
 //execute initialize function
